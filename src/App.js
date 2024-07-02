d35492baf0a265d36aed009b805581524885bf9d
Tushar Sharma <tttushar.789@gmail.com>
2024-07-02 12:46:55 +0530

    update

--- src/App.js
+++ src/App.js
@@ -29,26 +29,38 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
+            </Button>
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
               Next
-            </button>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step : {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ children, onClick, bgColor, textColor }) {
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: bgColor, color: textColor }}
+    >
+      {children}
+    </button>
   );
 }
 
